e0cc3d781986a212810a5a73c044c643b40d632b
bosskub1 <bossnakub223@gmail.com>
2026-02-02 22:39:26 +0700

    Update script URL for map ID 3351674303

--- CheckMapPremium.lua
+++ CheckMapPremium.lua
@@ -1,6 +1,6 @@
 local MAP_SCRIPTS = {
     [10595058975] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/ALPremium.lua",
-    [3351674303] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/DrivingEmpire.lua",
+    [3351674303] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/DM192914914.lua",
     [1122334455] = "https://raw.githubusercontent.com/bosskub1/EVE/Script3.lua",
 }
 

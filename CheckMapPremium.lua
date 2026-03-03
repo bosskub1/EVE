e6d2d1e285a5ee40faa47844598bc41885d747f3
bosskub1 <bossnakub223@gmail.com>
2026-03-03 16:42:17 +0700

    Add new script URL for PlaceId 77747658251236

--- CheckMapPremium.lua
+++ CheckMapPremium.lua
@@ -2,6 +2,7 @@ local MAP_SCRIPTS = {
     [10595058975] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/ALPremium.lua",
     [3351674303] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/DM192914914.lua",
     [86111605798689] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/SF3142121515151241.lua",
+    [77747658251236] = "https://raw.githubusercontent.com/bosskub1/EVE/refs/heads/main/77747658251236.lua",
 }
 
 
